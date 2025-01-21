df791f0f81052ef8421b6449e040dab266fa8b5c
juxatope <32027055+juxatope@users.noreply.github.com>
2025-01-21 01:07:43 -0500

    Update creds.sh

--- creds.sh
+++ creds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CSV_FILE="/root/aws_credentials.csv"
-ACCESS_KEY=$(awk -F, 'NR==2 {print $1}' $CSV_FILE)
-SECRET_KEY=$(awk -F, 'NR==2 {print $2}' $CSV_FILE)
+CSV_FILE="/root/aws.csv"
+ACCESS_KEY=$(awk -F, 'NR==2 {print $1}' $CSV_FILE | tr -d '\r')
+SECRET_KEY=$(awk -F, 'NR==2 {print $2}' $CSV_FILE | tr -d '\r') 
 echo "export AWS_ACCESS_KEY_ID=$ACCESS_KEY" >> /root/.bashrc
 echo "export AWS_SECRET_ACCESS_KEY=$SECRET_KEY" >> /root/.bashrc
 echo "export AWS_REGION=eu-north-1" >> /root/.bashrc
